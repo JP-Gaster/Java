4593d29612c166fff27d153e3fa555ff3c3c11ae
J. P. Gaster <128156817+JP-Gaster@users.noreply.github.com>
2024-10-31 08:19:59 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,12 @@
+class Filmes {
+  public static void main(String[] args) {
+    System.out.println("Esse é o Screen Match");
+    System.out.println("Filme: Transsformers: O Início");
+    
+    int ano = 2024;
+    boolean incluindo_no_plano = true;
+    double nota_filme = 4.9;
+    
+    System.out.println("Ano de lançamento " + ano);
+  }
+}
