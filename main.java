122892f887ab6e83c2dc6bc300243e86baa06a86
J. P. Gaster <128156817+JP-Gaster@users.noreply.github.com>
2024-11-06 08:59:31 -0300

    Add files via upload

--- main.java
+++ main.java
@@ -8,5 +8,12 @@ class Filmes {
     double nota_filme = 4.9;
     
     System.out.println("Ano de lançamento " + ano);
+    
+    double media = (4.9 + 4.0)/2;
+    System.out.println("A média das notas deste filme é:" +media);
+    
+    String sinopse;
+    sinopse = "A história que nunca antes havia sido contada. A origem de Optimus Prime e Megatron, duas figuras lendárias que em sua juventude foram irmãos de armas, lutando juntos pelo futuro de seu planeta natal, Cybertron.";
+    System.out.println(sinopse);
   }
 }
